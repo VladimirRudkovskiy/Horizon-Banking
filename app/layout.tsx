1aa3ca9b338fa39f204b9874a4c3dee583b3eab8
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-09-28 13:17:43 +0300

    startup

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+import { Import } from "lucide-react";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+	subsets: ['latin'],
+	weight: ['400', '700'],
+	variable: '--font-ibm-plex-serif'
+})
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Horizon",
+  description: "Horizon is a modern banking platform for everyone",
+	icons: {
+		icon: '/icons/logo.svg'
+	}
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
