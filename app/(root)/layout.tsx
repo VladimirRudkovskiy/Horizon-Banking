e0878f12bfbcd4a77fe6cc024df55d5f9aed3409
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-09-29 17:10:35 +0300

    sidebar styling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from "@/components/Sidebar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+	const loggedIn = { firstName: 'Adrian', lastname: 'JSM' };
+
+
   return (
-		<main>
-			SIDEBAR
+		<main className="flex h-screen w-full font-inter">
+			<Sidebar user={loggedIn}/>
 			{children}
 		</main>
   );
