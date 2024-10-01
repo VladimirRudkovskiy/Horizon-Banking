219878734dfa01818839cccdfb399085b621b198
Vladimir Rudkovskiy <rudkovskyvladimir21051@gmail.com>
2024-10-01 17:20:13 +0300

    implimenting complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-	const loggedIn = { firstName: 'Vladimir' };
+	const loggedIn = { firstName: 'Vladimir', lastName: 'North', email: 'rudkovskyvladimir21051@gmail.com' };
 	return (
 		<section className='home'>
 			<div className='home-content'>
@@ -21,7 +22,16 @@ const Home = () => {
 					totalCurrentBalance={ 1250.35 }
 					/>
 				</header>
+
+				RECENT TRANSACTIONS
 			</div>
+
+			<RightSidebar 
+			user={loggedIn}
+			transactions={[]}
+			banks={[{ currentBalance: 123.50 }, {currentBalance: 500.00}]}
+			/>
+
 		</section>
 	)
 }
